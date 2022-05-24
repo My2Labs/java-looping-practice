1236e07f5ac8fdc7409b43775af91f0add847488
My2Labs <sking171@ford.com>
2022-05-24 07:01:46 -0500

    Work exercise.

--- App.java
+++ App.java
@@ -1,14 +1,34 @@
 class App {
-  public static void main(String args[]){
+  public static void main(String args[]) {
     App.forLoop();
     App.whileLoop();
     App.doWhileLoop();
   }
 
   private static void forLoop() {
+    int i = 10;
+    while (i > 0) {
+      System.out.println(i);
+      i--;
+    }
+    System.out.println("Launch!");
   }
+
   private static void whileLoop() {
+    int i = 0;
+    System.out.println("Make whileLoop print the statement 'I love Java' 5 times:");
+    while (i < 5) {
+      System.out.println("I love Java!");
+      i++;
+    }
   }
+
   private static void doWhileLoop() {
+    int i = 0;
+    System.out.println("Make doWhileLoop print the statement 'I love Java' 5 times:");
+    while (i < 5) {
+      System.out.println("I love Java!");
+      i++;
+    }
   }
 }
